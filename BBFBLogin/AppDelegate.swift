9762dd9340f396f3e9d2f8fa0691b48448971f26
Bryan A Bolivar M <boli@Mac-mini-de-Bryan.local>
2015-03-25 14:30:39 -0500

    rintegrated with facebook-ios SDK 4.0

--- BBFBLogin/AppDelegate.swift
+++ BBFBLogin/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -35,7 +36,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
+
     }
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
--- BBFBLogin/ViewController.swift
+++ BBFBLogin/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var loginButton:FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center;
+        self.view.addSubview(loginButton)
+
     }
 
     override func didReceiveMemoryWarning() {
